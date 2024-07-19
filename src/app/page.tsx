c04ff2fba6e8d8d9549005e4ff291f2c63d174c1
KolesnikovTimurWeb <kolesnikovtimyrweb@gmail.com>
2024-07-19 14:52:58 +0300

    Fixed page pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,69 @@
-import Image from "next/image";
-import JobListItem from "@/components/JobListItem";
-import JobFilterSideBar from "@/components/JobFilterSideBar";
+import JobFilterSidebar from "@/components/JobFilterSideBar";
 import JobResults from "@/components/JobResults";
-import { JobFilterValues } from "@/lib/validation";
 import H1 from "@/components/ui/H1";
+import { JobFilterValues } from "@/lib/validation";
 import { Metadata } from "next";
 
-interface PageProps{
-  searchParams:{
-    q?:string,
-    type?:string,
-    location?:string,
-    remote?:string,
-  }
-}
-function getTitle({q,type,location,remote}:JobFilterValues){
-  const title = q ? `${q} jobs`: type ? `${type} developer jobs` : remote ? `Remote developer jobs` : "All developer jobs"
-
-  const titleLocation = location ? `in ${location}` : ""
-
-  return `${title}${titleLocation}`
+interface PageProps {
+  searchParams: {
+    q?: string;
+    type?: string;
+    location?: string;
+    remote?: string;
+    page?: string;
+  };
 }
 
-export function generateMetadata({searchParams:{q,type,location,remote}}:PageProps):Metadata{
-  return{
-    title:getTitle({q,type,location,remote : remote==="true"})
-  }
+function getTitle({ q, type, location, remote }: JobFilterValues) {
+  const titlePrefix = q
+    ? `${q} jobs`
+    : type
+      ? `${type} developer jobs`
+      : remote
+        ? "Remote developer jobs"
+        : "All developer jobs";
+
+  const titleSuffix = location ? ` in ${location}` : "";
+
+  return `${titlePrefix}${titleSuffix}`;
 }
 
-export default async function Home({searchParams:{q,type,location,remote}}:PageProps) {
-  const filterValues:JobFilterValues = {
+export function generateMetadata({
+  searchParams: { q, type, location, remote },
+}: PageProps): Metadata {
+  return {
+    title: `${getTitle({
+      q,
+      type,
+      location,
+      remote: remote === "true",
+    })} | Flow Jobs`,
+  };
+}
+
+export default async function Home({
+  searchParams: { q, type, location, remote, page },
+}: PageProps) {
+  const filterValues: JobFilterValues = {
     q,
     type,
     location,
     remote: remote === "true",
-  }
+  };
+
   return (
-    <main className="max-w-5xl m-auto px-3 my-10 space-y-10">
+    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
-        <H1 className="text-4xl font-extrabold tracking-tight  lg:text-5xl">{getTitle(filterValues)}</H1>
-        <p className="text-muted-foreground">Find your dream job</p>
+        <H1>{getTitle(filterValues)}</H1>
+        <p className="text-muted-foreground">Find your dream job.</p>
       </div>
-      <section className="flex flex-col md:flex-row gap-4">
-      <JobFilterSideBar defaultValues={filterValues}/>
-      <JobResults filterValues={filterValues}/>
+      <section className="flex flex-col gap-4 md:flex-row">
+        <JobFilterSidebar defaultValues={filterValues} />
+        <JobResults
+          filterValues={filterValues}
+          page={page ? parseInt(page) : undefined}
+        />
       </section>
-
-
     </main>
   );
 }
--- src/components/JobListItem.tsx
+++ src/components/JobListItem.tsx
@@ -1,72 +1,70 @@
-
-import prisma from "@/lib/prisma";
-import Image from "next/image";
-import companyPlaceholderLogo from "@/assets/company-logo-placeholder.png";
-import {BanknoteIcon, Briefcase, Clock, Globe2, MapPin} from "lucide-react"
+import companyLogoPlaceholder from "@/assets/company-logo-placeholder.png";
 import { formatMoney, relativeDate } from "@/lib/utils";
-import Badge from "./Badge";
 import { Job } from "@prisma/client";
+import { Banknote, Briefcase, Clock, Globe2, MapPin } from "lucide-react";
+import Image from "next/image";
+import Badge from "./Badge";
 
-interface JobListItemProps{
-  job:Job
+interface JobListItemProps {
+  job: Job;
 }
 
-export default async function JobListItem({job:{
-  title,
-  companyName,
-  type,
-  locationType,
-  location,
-  salary,
-  companyLogoUrl,
-  createdAt,
-}}:JobListItemProps) {
-   return (
-    <article className="flex gap-3 border rounded-lg p-5 hover:bg-muted/60">
+export default function JobListItem({
+  job: {
+    title,
+    companyName,
+    type,
+    locationType,
+    location,
+    salary,
+    companyLogoUrl,
+    createdAt,
+  },
+}: JobListItemProps) {
+  return (
+    <article className="flex gap-3 rounded-lg border p-5 hover:bg-muted/60">
       <Image
-       src={companyLogoUrl || companyPlaceholderLogo}
+        src={companyLogoUrl || companyLogoPlaceholder}
         alt={`${companyName} logo`}
         width={100}
         height={100}
-        className="rounded-lg self-center"
-        /> 
+        className="self-center rounded-lg"
+      />
       <div className="flex-grow space-y-3">
         <div>
           <h2 className="text-xl font-medium">{title}</h2>
           <p className="text-muted-foreground">{companyName}</p>
         </div>
         <div className="text-muted-foreground">
-          <p className="flex items-center gap-1.5 sm:hidden "> 
-            <Briefcase size={16} className="shrink-0"/>
+          <p className="flex items-center gap-1.5 sm:hidden">
+            <Briefcase size={16} className="shrink-0" />
             {type}
           </p>
-          <p className="flex items-center gap-1.5 "> 
-            <MapPin size={16} className="shrink-0"/>
+          <p className="flex items-center gap-1.5">
+            <MapPin size={16} className="shrink-0" />
             {locationType}
           </p>
-          <p className="flex items-center gap-1.5 "> 
-            <Globe2 size={16} className="shrink-0"/>
+          <p className="flex items-center gap-1.5">
+            <Globe2 size={16} className="shrink-0" />
             {location || "Worldwide"}
           </p>
-          <p className="flex items-center gap-1.5 "> 
-            <BanknoteIcon size={16} className="shrink-0"/>
+          <p className="flex items-center gap-1.5">
+            <Banknote size={16} className="shrink-0" />
             {formatMoney(salary)}
           </p>
-          <p className="flex items-center gap-1.5 sm:hidden"> 
-            <Clock size={16} className="shrink-0"/>
+          <p className="flex items-center gap-1.5 sm:hidden">
+            <Clock size={16} className="shrink-0" />
             {relativeDate(createdAt)}
           </p>
         </div>
       </div>
-      <div className="hidden sm:flex flex-col shrink-0 items-end justify-between"> 
-        <Badge>
-          {type}
-        </Badge>
-        <span className="flex items-center gap-1.5 text-muted-foreground"> 
-          <Clock size={16}/>
+      <div className="hidden shrink-0 flex-col items-end justify-between sm:flex">
+        <Badge>{type}</Badge>
+        <span className="flex items-center gap-1.5 text-muted-foreground">
+          <Clock size={16} />
           {relativeDate(createdAt)}
         </span>
       </div>
     </article>
-   );
- }
+  );
+}
